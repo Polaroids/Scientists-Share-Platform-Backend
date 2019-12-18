f612d71d3622094effab67bf6479b8c5d22b784f
SeaCat <1404455176@qq.com>
2019-12-18 14:46:19 +0800

    191218
    Signed-off-by: SeaCat <1404455176@qq.com>

--- src/main/java/org/buaa/platform/controller/UserController.java
+++ src/main/java/org/buaa/platform/controller/UserController.java
@@ -2,12 +2,19 @@ package org.buaa.platform.controller;
 
 
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.filters.CorsFilter;
 import org.buaa.platform.service.UserService;
 import org.buaa.platform.tool.RetResponse;
 import org.buaa.platform.tool.RetResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @RestController
@@ -17,7 +24,8 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping({"login"})
-    public RetResult<Object> login(String userName, String password){
+    public RetResult<Object> login(String userName, String password, HttpServletResponse response){
+    	response.setHeader("Access-Control-Allow-Origin", "*");
         try {
             if (userService.login(userName, password)) {
                 return RetResponse.makeOKRsp();
@@ -30,8 +38,9 @@ public class UserController {
     }
     
     @RequestMapping({"register"})
-    public RetResult<Object> register(String userName, String password, String email){
-        try {   	
+    public RetResult<Object> register(String userName, String password, String email, HttpServletResponse response){
+    	response.setHeader("Access-Control-Allow-Origin", "*");
+    	try {   	
             if (userService.register(userName, password, email)) {
                 return RetResponse.makeOKRsp();
             }
